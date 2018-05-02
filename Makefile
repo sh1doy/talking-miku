1b98cf2d446f06f0a2a7922eac77bc732fc04743
紫藤佑介 <chahan69@gmail.com>
2018-05-03 02:37:37 +0900

    Add Makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+TMP_DIR = dataset/tmp
+NUCC_URL = http://mmsrv.ninjal.ac.jp/nucc/nucc.zip
+NUCC_ZIP = nucc.zip
+
+dataset-nucc:
+	wget -N -P $(TMP_DIR) $(NUCC_URL)
+	unzip -u $(TMP_DIR)/$(NUCC_ZIP) -d $(TMP_DIR)
+	python src/dataset_nucc.py
---
+++ src/dataset_nucc.py
@@ -0,0 +1,41 @@
+from glob import glob
+from tqdm import tqdm
+import re
+
+
+files = sorted(glob("./dataset/tmp/nucc/*.txt"))
+
+
+def clean(file):
+    with open(file, "r") as f:
+        data = f.readlines()
+        while 1:
+            if data[0][0] in ["F", "M", "X"] and data[0][4] == "：":
+                break
+            data = data[1:]
+        for i in range(len(data)):
+            if data[i][0] in ["＠", "@"]:
+                data = data[:i]
+                break
+        data = "".join(data).replace("\r", "").replace("\n", "")
+        data = re.sub(r"[F|M][0-9]{3}：+", "\n", data)
+        data = re.sub(r"Ｘ：", "\n", data)
+        data = re.sub(r"X：", "\n", data)
+        data = re.sub(r"％ｃｏｍ.*", "", data)
+        data = re.sub(r"ＭＳ：", "\n", data)
+        data = re.sub(r"＜.*＞", "", data)
+        data = re.sub(r"１：＊＊＊。\n", "", data)
+        data = re.sub(r"（.*）", "", data)
+        data = re.sub(r"【.*】", "", data)
+        data = re.sub(r"：", "", data)
+        data = re.sub(r"\u3000", "", data)
+        data = re.sub(r"[F|M][0-9]{3}", "P", data)
+        data = re.sub(r"＊", "", data)
+        return "\n".join([line for line in data.split("\n") if line not in ["", "。"]])
+
+
+data = [clean(file) for file in tqdm(files, "cleaning data")]
+
+for e, d in enumerate(data):
+    with open("./dataset/conversation/nucc_{:03d}.txt".format(e), "w") as f:
+        f.writelines(d)
