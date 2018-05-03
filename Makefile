a85e5256051e8bfbda9ed7608bae6b5f98dc3729
紫藤佑介 <chahan69@gmail.com>
2018-05-03 20:53:27 +0900

    Add miku script

--- Makefile
+++ Makefile
@@ -6,3 +6,13 @@ dataset-nucc:
 	wget -N -P $(TMP_DIR) $(NUCC_URL)
 	unzip -u $(TMP_DIR)/$(NUCC_ZIP) -d $(TMP_DIR)
 	python src/dataset_nucc.py
+
+dataset-miku:
+	python src/dataset_miku.py 
+
+dataset:
+	dataset-nucc
+	dataset-miku
+
+
+
---
+++ src/dataset_miku.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+seq = []
+print("Fetching", end="")
+for i in range(1, 1000):
+    sleep(1.0)
+    url = "https://icws.indigo-bell.com/search?q=前川みく&st=n&page=" + str(i)
+    responce = requests.get(url)
+    print(".", end="")
+    soup = BeautifulSoup(responce.text, "lxml")
+    lines = soup.find("tbody", class_="result").find_all("tr")
+    if lines == []:
+        break
+    for line in lines:
+        contents = line.find_all("td")[-1].contents
+        seq += contents
+print("\nGot {} lines.".format(len(seq)))
+seq = [line.replace("○○", "P") for line in seq]
+with open("./dataset/charactor/miku.txt", "w") as f:
+    f.write("\n".join(seq))
